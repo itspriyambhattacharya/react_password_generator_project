deb03561d1fb5435a55666e5635918f1a2da3418
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-09 20:45:41 +0530

    Add password state initialization in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("Bhgrzxxs");
 
   return (
     <>
