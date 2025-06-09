b6518246c4e024d4e5aa2814e17648e7987da2a6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-09 20:44:31 +0530

    Refactor App component state management for password generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
 
   return (
     <>
