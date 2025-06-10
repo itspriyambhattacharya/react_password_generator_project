82bfcc1e82618a16b76aea9fb4866d0aa4afe4f6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-10 14:27:10 +0530

    Implement clipboard copy functionality for generated password

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,9 @@ function App() {
     setPassword(pass);
   }, [length, numberAllowed, characterAllowed]);
 
-  const copytoClipboard = useCallback(() => {}, [password]);
+  const copytoClipboard = useCallback(() => {
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
   useEffect(() => {
     passwordGenerator();
